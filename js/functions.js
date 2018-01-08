6607f91a2f9e30eb79c43aa45c4a9f196a64a002
Alejandro Sobko <alee-sk8@hotmail.com>
2018-01-07 23:41:20 -0300

    Change the site completely

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,7 @@
-$(document).ready(function() {
-  $("#back-top").hide();
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
 
-  $('.nav a, .button').click(function() {
-    var item = $(this).attr("href");
-    var destination = $(item).offset().top;
-    $("html, body").animate({ scrollTop: destination }, 1200);
-    return false;
-  });
-
-  var height = $( document ).height();
-  var scroll = (height/100)*20;
-  $(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > scroll) {
-        $('#back-top').fadeIn();
-      } else {
-        $('#back-top').fadeOut();
-      }
-    });
-
-    $('#back-top a').click(function () {
-      $('body,html').animate({
-        scrollTop: 0
-      }, 850);
-      return false;
-    });
-  });
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
 });
