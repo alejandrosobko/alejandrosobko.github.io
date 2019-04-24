33ec086f924f0de290e13cc5d9832a2c088ec06b
Alejandro Sobko <alejandro.sobko@gmail.com>
2019-04-24 20:26:28 -0300

    Reduce time of animation and add better styles

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
+        scrollTop: $($.attr(this, 'href')).offset().top - 20
     }, 'slow');
 
     $('label.hamburguer-menu input').prop('checked', false);
