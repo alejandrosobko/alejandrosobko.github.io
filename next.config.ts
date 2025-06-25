ffb5e352b1f127c918c90214d5ef686dddea657a
Alejandro Sobko <alejandro.sobko@gmail.com>
2025-06-24 21:44:32 -0300

    new config for prod

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next"
 
+const REPO_NAME = "alejandrosobko.github.io"
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
@@ -10,6 +12,8 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ["@phosphor-icons/react", "@chakra-ui/react"],
   },
   transpilePackages: ["@chakra-ui/react", "@phosphor-icons/react"],
+  basePath: process.env.NODE_ENV === "production" ? `/${REPO_NAME}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${REPO_NAME}` : "",
 }
 
 export default nextConfig
